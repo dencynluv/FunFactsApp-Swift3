6a7e05748bca2f8aca5e4fccb52e310c2c1743ae
Cynthia Soto <sotocyn@gmail.com>
2016-12-12 21:27:55 -0800

    Created a stored property for the label and assigned a new string to it upon first loading the app. Added comments for what I did.

--- FunFacts/ViewController.swift
+++ FunFacts/ViewController.swift
@@ -10,9 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  //creating an IBOutlet (Interface Builder Outlet) from the label in our View in the Story Board to our View Controller & created a stored property to manage it
+  @IBOutlet weak var funFactLabel: UILabel!
+  
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    // Do any additional setup after loading the view
+    
+    //Assigned a string to the text property of the label in the viewDidLoad() method so upon launching the app this code is executed first and the text in our label will change to the one below
+    funFactLabel.text = "An interesting fact!"
   }
 
   override func didReceiveMemoryWarning() {
